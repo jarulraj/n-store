a5a3c8c73c2a8d7c0fd105bee4cb4eaa5eca977c
jarulraj <jarulraj@cs.cmu.edu>
2014-05-25 00:26:52 -0400

    Testing gettime

--- scratch/time.cpp
+++ scratch/time.cpp
@@ -2,14 +2,28 @@
 #include <chrono>
 #include <ctime>
 
-long fibonacci(int n)
-{
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+timespec diff(timespec start, timespec end){
+	timespec temp;
+	if ((end.tv_nsec-start.tv_nsec)<0) {
+		temp.tv_sec = end.tv_sec-start.tv_sec-1;
+		temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+	} else {
+		temp.tv_sec = end.tv_sec-start.tv_sec;
+		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+	}
+	return temp;
+}
+
+long fibonacci(int n){
     if (n < 3) return 1;
     return fibonacci(n-1) + fibonacci(n-2);
 }
 
-int main()
-{
+int main(){
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
     std::cout << "f(42) = " << fibonacci(42) << '\n';
@@ -27,4 +41,15 @@ int main()
     std::chrono::duration<double> hr_elapsed_seconds = hr_end-hr_start;
 
     std::cout << "hr_elapsed time: " << hr_elapsed_seconds.count() << "s\n";
+
+    timespec time1, time2;
+	int temp;
+
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    std::cout << "f(42) = " << fibonacci(42) << '\n';
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+
+	std::cout << diff(time1, time2).tv_sec << ":" << diff(time1, time2).tv_nsec << endl;
+	return 0;
+
 }
