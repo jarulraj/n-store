586e50ea735b60f07dad22f407a60c036e117be5
jarulraj <jarulraj@cs.cmu.edu>
2014-07-31 13:07:44 -0400

    Checkpoint

--- eval.py
+++ eval.py
@@ -109,6 +109,7 @@ SDV_SCRIPT = SDV_DIR + "/ivt_pm_sdv.sh"
 NSTORE = "./src/nstore"
 FS_PATH = "/mnt/pmfs/n-store/"
 PMEM_CHECK = "./src/pmem_check"
+PERF = "perf"
 
 SYSTEMS = ("wal", "sp", "lsm", "opt_wal", "opt_sp", "opt_lsm")
 LATENCIES = ("200", "800")
@@ -122,7 +123,7 @@ YCSB_RW_MIXES = [0, 0.5]
 
 YCSB_PERF_DIR = "../results/ycsb/performance/"
 YCSB_STORAGE_DIR = "../results/ycsb/storage/"
-
+YCSB_NVM_DIR = "../results/ycsb/nvm/"
 
 
 ###################################################################################                   
@@ -271,6 +272,80 @@ def create_ycsb_storage_bar_chart(datasets, workload_mix):
         
     return (fig)
 
+def create_ycsb_nvm_bar_chart(datasets, workload_mix):
+    fig = plot.figure()
+    ax1 = fig.add_subplot(111)
+     
+    labels = ("WAL-2X", "SP-2X", "LSM-2X",
+              "PM-WAL-2X", "PM-SP-2X", "PM-LSM-2X")
+
+    x_values = YCSB_SKEW_FACTORS
+    N = len(x_values)
+    x_labels = ["Low", "High"]
+
+    ind = np.arange(N)  
+    width = 0.05  # the width of the bars
+    offset = 0.15
+    
+    for group in xrange(len(datasets)):
+        # GROUP
+        l_misses = []       
+        s_misses = [] 
+
+        for line in  xrange(len(datasets[group])):
+            for col in  xrange(len(datasets[group][line])):
+                if col == 1:
+                    l_misses.append(datasets[group][line][col] / (1024))
+                if col == 2:
+                    s_misses.append(datasets[group][line][col] / (1024))
+  
+        LOG.info("%s l_misses = %s s_misses = %s ", labels[group], str(l_misses), str(s_misses))
+                
+        ax1.bar(ind + group * width, l_misses, width, color=OPT_COLORS[group])
+        ax1.bar(ind + group * width, s_misses, width, bottom=l_misses, color=OPT_COLORS[group], hatch='/')
+        
+    # GRID
+    axes = ax1.get_axes()
+    if workload_mix == "read-only":
+        axes.set_ylim(0, 100000)
+    elif workload_mix == "read-heavy":
+        axes.set_ylim(0, 100000)
+    elif workload_mix == "write-heavy":
+        axes.set_ylim(0, 100000)
+        
+    makeGrid(ax1)
+    
+    # LEGEND
+    fp = FontProperties(family=OPT_FONT_NAME, weight=OPT_LABEL_WEIGHT)
+    ax1.legend(labels,
+                prop=fp,
+                bbox_to_anchor=(0.0, 1.1, 1.0, 0.10),
+                loc=1,
+                ncol=2,
+                mode="expand",
+                shadow=OPT_LEGEND_SHADOW,
+                borderaxespad=0.0,
+    )
+    
+    # Y-AXIS
+    ax1.set_ylabel("NVM Accesses", fontproperties=fp)
+    ax1.yaxis.set_major_locator(MaxNLocator(5))
+    ax1.minorticks_on()
+    for tick in ax1.yaxis.get_major_ticks():
+        tick.label.set_fontname(OPT_FONT_NAME)
+        
+    # X-AXIS
+    ax1.set_xlabel("Skew", fontproperties=fp)
+    ax1.minorticks_on()
+    ax1.set_xticklabels(x_labels)
+    print (x_values)
+    ax1.set_xticks(ind + width * len(x_labels))
+    print(x_labels)
+    for tick in ax1.xaxis.get_major_ticks():
+        tick.label.set_fontname(OPT_FONT_NAME)
+        
+    return (fig)
+
 # YCSB PERF -- PLOT
 def ycsb_perf_plot():
     for workload in YCSB_WORKLOAD_MIX:    
@@ -299,6 +374,23 @@ def ycsb_storage_plot():
                         
         fileName = "ycsb-storage-%s.pdf" % (workload)
         saveGraph(fig, fileName, width=OPT_GRAPH_WIDTH, height=OPT_GRAPH_HEIGHT)
+
+# YCSB NVM -- PLOT               
+def ycsb_nvm_plot():    
+    for workload in YCSB_WORKLOAD_MIX:   
+        for lat in LATENCIES:
+ 
+            datasets = []
+        
+            for sy in SYSTEMS:    
+                dataFile = loadDataFile(2, 3, os.path.realpath(os.path.join(YCSB_NVM_DIR, sy + "/" + workload + "/" + lat + "/nvm.csv")))
+                datasets.append(dataFile)
+                       
+            fig = create_ycsb_nvm_bar_chart(datasets, workload)
+                            
+            fileName = "ycsb-nvm-%s.pdf" % (workload)
+            saveGraph(fig, fileName, width=OPT_GRAPH_WIDTH, height=OPT_GRAPH_HEIGHT)
+
                    
 ###################################################################################                   
 # EVAL                   
@@ -549,7 +641,7 @@ def ycsb_storage_eval(log_name):
         
     rw_mixes = YCSB_RW_MIXES
     skew_factors = YCSB_SKEW_FACTORS
-    engines =  ENGINES   
+    engines = ENGINES   
 
     # LOG RESULTS
     log_file = open(log_name, 'w')
@@ -570,7 +662,135 @@ def ycsb_storage_eval(log_name):
                 cleanup()
                 subprocess.call([NSTORE, '-k', str(keys), '-x', str(txns), '-p', str(rw_mix), '-q', str(skew_factor), eng], stdout=log_file)
                 get_stats(eng, rw_mix, skew_factor)
-                             
+
+
+# YCSB NVM -- EVAL
+def ycsb_nvm_eval(log_name):            
+    subprocess.call(['rm', '-rf', YCSB_NVM_DIR])          
+    keys = YCSB_KEYS 
+    txns = YCSB_TXNS
+
+    PERF_STAT = "stat"    
+    PERF_STAT_FLAGS = "-e LLC-load-misses,LLC-store-misses"
+
+     # CLEANUP
+    def cleanup():
+        subprocess.call(["rm -f " + FS_PATH + "./*"], shell=True)        
+
+    nvm_latencies = LATENCIES                
+    rw_mixes = YCSB_RW_MIXES
+    skew_factors = YCSB_SKEW_FACTORS
+    engines = ENGINES   
+
+    # LOG RESULTS
+    log_file = open(log_name, 'w')
+    
+    for nvm_latency in nvm_latencies:
+
+        ostr = ("LATENCY %s \n" % nvm_latency)    
+        print (ostr, end="")
+        log_file.write(ostr)
+        log_file.flush()
+        
+        if enable_sdv :
+            cwd = os.getcwd()
+            os.chdir(SDV_DIR)
+            subprocess.call(['sudo', SDV_SCRIPT, '--enable', '--pm-latency', str(nvm_latency)], stdout=log_file)
+            os.chdir(cwd)
+               
+        # RW MIX
+        for rw_mix  in rw_mixes:
+            # SKEW FACTOR
+            for skew_factor  in skew_factors:
+                ostr = ("--------------------------------------------------- \n")
+                print (ostr, end="")
+                log_file.write(ostr)
+                ostr = ("RW MIX :: %.1f SKEW :: %.2f \n" % (rw_mix, skew_factor))
+                print (ostr, end="")
+                log_file.write(ostr)                    
+                log_file.flush()
+        
+                for eng in engines:
+                    cleanup()
+                    subprocess.call([PERF, PERF_STAT, PERF_STAT_FLAGS, NSTORE,
+                                     '-k', str(keys), '-x', str(txns), '-p', str(rw_mix), '-q', str(skew_factor), eng],
+                                    stdout=log_file, stderr=log_file)
+                                  
+    log_file.close()   
+    log_file = open(log_name, "r")    
+
+    # CLEAN UP RESULT DIR
+    subprocess.call(['rm', '-rf', YCSB_NVM_DIR])          
+ 
+    latency = 0
+    rw_mix = 0.0
+    skew = 0.0    
+    
+    for line in log_file:
+        if "LATENCY" in line:
+            entry = line.strip().split(' ');
+            if entry[0] == "LATENCY":
+                latency = entry[1]
+                    
+        if "RW MIX" in line:
+            entry = line.strip().split(' ');
+            print("RW MIX :: " + str(entry))
+            rw_mix = entry[3]
+            skew = entry[6]
+                        
+            if(rw_mix == '0.0'):
+                workload_type = 'read-only'
+            elif(rw_mix == '0.1'):
+                workload_type = 'read-heavy'
+            elif(rw_mix == '0.5'):
+                workload_type = 'write-heavy'  
+                       
+        if "Throughput" in line:
+            entry = line.strip().split(':');
+            etypes = entry[0].split(' ');
+            
+            if(etypes[0] == "WAL"):
+                engine_type = "wal"                
+            elif(etypes[0] == "SP"):
+                engine_type = "sp"
+            elif(etypes[0] == "LSM"):
+                engine_type = "lsm"
+            elif(etypes[0] == "OPT_WAL"):
+                engine_type = "opt_wal"
+            elif(etypes[0] == "OPT_SP"):
+                engine_type = "opt_sp"
+            elif(etypes[0] == "OPT_LSM"):
+                engine_type = "opt_lsm"
+                                                        
+        if "LLC-load-misses" in line:
+            entry = line.strip().split(' ');
+            if(entry[0] == '<not'):
+                llc_l_miss = "0"
+            else:    
+                llc_l_miss = str(entry[0])
+            llc_l_miss = llc_l_miss.replace(",", "")    
+                
+            print(engine_type + ", " + str(rw_mix) + " , " + str(skew) + " , " + str(latency) + " :: " + str(llc_l_miss) + "\n")
+
+
+        if "LLC-store-misses" in line:
+            entry = line.strip().split(' ');
+            if(entry[0] == '<not'):
+                llc_s_miss = "0"
+            else:    
+                llc_s_miss = str(entry[0])
+            llc_s_miss = llc_s_miss.replace(",", "")    
+                
+            print(engine_type + ", " + str(rw_mix) + " , " + str(skew) + " , " + str(latency) + " :: " + str(llc_s_miss) + "\n")
+                                                                
+            result_directory = YCSB_NVM_DIR + engine_type + "/" + workload_type + "/" + latency + "/";
+            if not os.path.exists(result_directory):
+                os.makedirs(result_directory)
+
+            result_file_name = result_directory + "nvm.csv"
+            result_file = open(result_file_name, "a")
+            result_file.write(str(skew) + " , " + str(llc_l_miss) + " , " + str(llc_s_miss) + "\n")
+            result_file.close()    
                 
 ## ==============================================
 # # main
@@ -585,8 +805,10 @@ if __name__ == '__main__':
     parser.add_argument("-t", "--enable-trials", help='enable trials', action='store_true')
     parser.add_argument("-y", "--ycsb_perf_eval", help='evaluation', action='store_true')
     parser.add_argument("-s", "--ycsb_storage_eval", help='plot data', action='store_true')
+    parser.add_argument("-n", "--ycsb_nvm_eval", help='plot data', action='store_true')
     parser.add_argument("-p", "--ycsb_perf_plot", help='ycsb throughput', action='store_true')
     parser.add_argument("-q", "--ycsb_storage_plot", help='ycsb storage', action='store_true')
+    parser.add_argument("-r", "--ycsb_nvm_plot", help='ycsb storage', action='store_true')
     
     args = parser.parse_args()
     
@@ -597,6 +819,7 @@ if __name__ == '__main__':
 
     ycsb_eval_log_name = "ycsb_perf.log"
     ycsb_storage_log_name = "ycsb_storage.log"
+    ycsb_nvm_log_name = "ycsb_nvm.log"
     
     # YCSB PERF -- EVAL
     if args.ycsb_perf_eval:
@@ -606,6 +829,10 @@ if __name__ == '__main__':
     if args.ycsb_storage_eval:
         ycsb_storage_eval(ycsb_storage_log_name);
              
+    # YCSB NVM -- EVAL
+    if args.ycsb_nvm_eval:
+        ycsb_nvm_eval(ycsb_nvm_log_name);             
+             
     # YCSB PERF -- PLOT
     if args.ycsb_perf_plot:      
         ycsb_perf_plot();          
@@ -613,4 +840,7 @@ if __name__ == '__main__':
     # YCSB STORAGE -- PLOT               
     if args.ycsb_storage_plot:                
        ycsb_storage_plot();
-                   
+       
+   # YCSB NVM -- PLOT               
+    if args.ycsb_nvm_plot:                
+       ycsb_nvm_plot();                          
